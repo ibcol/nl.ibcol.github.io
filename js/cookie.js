254ed3f997fe0c6af67aabc251e25c30df0c4a63
RomanYuen <jasonyuen1001@gmail.com>
2020-05-28 12:53:43 +0800

    index cookie

--- js/cookie.js
+++ js/cookie.js
@@ -1,56 +1,43 @@
 $(document).ready(function () {
 
-    // console.log('x');
-    var user = getCookie("username");
-    if (user == "") {
-        setTimeout(function () {
-            $("#cookieConsent").fadeIn(0);
-        }, 4000);
-    } else {
+    setTimeout(function () {
+        $("#cookieConsent").fadeIn(0);
+    }, 4000);
 
-    }
 
     $("#closeCookieConsent, .cookieConsentOK").click(function () {
         $("#cookieConsent").fadeOut(200);
-        if (user != "" && user != null) {
-            setCookie("username", 'user', 30);
-        }
     });
 });
 
 
+// function setCookie(cname, cvalue, exdays) {
+//     var d = new Date();
+//     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+//     var expires = "expires=" + d.toGMTString();
+//     document.cookie = cname + "=" + cvalue + "; " + expires;
+// }
 
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
+// function getCookie(cname) {
+//     var name = cname + "=";
+//     var ca = document.cookie.split(';');
+//     for (var i = 0; i < ca.length; i++) {
+//         var c = ca[i].trim();
+//         if (c.indexOf(name) == 0) {
+//             return c.substring(name.length, c.length);
+//         }
+//     }
+//     return "";
+// }
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-function checkCookie() {
-    var user = getCookie("username");
-    if (user != "") {
-        alert("Welcome again " + user);
-    } else {
-        user = prompt("Please enter your name:", "");
-        if (user != "" && user != null) {
-            setCookie("username", user, 30);
-        }
-    }
-}
+// function checkCookie() {
+//     var user = getCookie("username");
+//     if (user != "") {
+//         alert("欢迎 " + user + " 再次访问");
+//     } else {
+//         user = prompt("请输入你的名字:", "");
+//         if (user != "" && user != null) {
+//             setCookie("username", user, 30);
+//         }
+//     }
+// }
