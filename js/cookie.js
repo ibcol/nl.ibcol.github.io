e86b1640fecaa586c3da79984aaf3be7cdf89409
RomanYuen <jasonyuen1001@gmail.com>
2020-05-28 13:12:06 +0800

    cookie test

--- js/cookie.js
+++ js/cookie.js
@@ -1,43 +1,36 @@
 $(document).ready(function () {
 
-    setTimeout(function () {
-        $("#cookieConsent").fadeIn(0);
-    }, 4000);
-
+    console.log(getCookie("closed"));
+    if (getCookie("closed") == "closed") {
+ 
+    } else {
+        setTimeout(function () {
+            $("#cookieConsent").fadeIn(0);
+        }, 4000);
+    }
 
     $("#closeCookieConsent, .cookieConsentOK").click(function () {
         $("#cookieConsent").fadeOut(200);
+        $("cookieConsent").remove();
+        setCookie("closed", "closed", 365)
     });
 });
 
 
-// function setCookie(cname, cvalue, exdays) {
-//     var d = new Date();
-//     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-//     var expires = "expires=" + d.toGMTString();
-//     document.cookie = cname + "=" + cvalue + "; " + expires;
-// }
-
-// function getCookie(cname) {
-//     var name = cname + "=";
-//     var ca = document.cookie.split(';');
-//     for (var i = 0; i < ca.length; i++) {
-//         var c = ca[i].trim();
-//         if (c.indexOf(name) == 0) {
-//             return c.substring(name.length, c.length);
-//         }
-//     }
-//     return "";
-// }
-
-// function checkCookie() {
-//     var user = getCookie("username");
-//     if (user != "") {
-//         alert("欢迎 " + user + " 再次访问");
-//     } else {
-//         user = prompt("请输入你的名字:", "");
-//         if (user != "" && user != null) {
-//             setCookie("username", user, 30);
-//         }
-//     }
-// }
+var setCookie = function(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+  }
+  
+  var getCookie = function(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1);
+      if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+  }
